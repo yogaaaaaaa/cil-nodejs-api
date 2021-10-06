ab8ab3e4129b83d14f3cf94a50a6f744b1e89056
yogaaaaaaa <yogaadhi@icloud.com>
2021-10-06 11:05:43 +0700

    adding delete user

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: true}))
 
+
+//create new user
 app.post('/user', (req, res)=>{
     res.send({
         message: 'CREATE NEW USER: POST /user',
@@ -14,22 +16,35 @@ app.post('/user', (req, res)=>{
     })
 })
 
+
+//get all user
 app.get('/user/', (req,res)=>{
     res.send('GET USER LIST: GET /user/')
 })
 
+
+//get user by id
 app.get('/user/:id', (req, res)=>{
     res.send('GET USER: GET /user/'+ req.params.id);
 })
 
+
+//update user
 app.patch('/user/:id', (req, res)=>{
     const msg = {
-        message: 'update user: PATCH /user/'+ req.params.id,
+        message: 'UPDATE USER: PATCH /user/'+ req.params.id,
         body:req.body
     };
     res.send(msg);
 })
 
+//delete user
+app.delete('/user/delete/:id', (req, res)=>{
+    res.send('DELETE USER: DELETE /user/'+ req.params.id)
+})
+
+
+//helloworld
 app.get('/helloworld', (req, res)=>{
     res.send('hello world!')
 })
